4e5582b1c63bd8e5a22670c2062ff62be02f2d66
tiivoooo <tienv5995@gmail.com>
2024-10-06 16:12:59 +0700

    get transaction

---
+++ StudySpace.Service/BusinessModel/DashboardSupplierModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudySpace.Service.BusinessModel
+{
+    public class DashboardSupplierModel
+    {
+        public string StoreName { get; set; }
+
+        public Double TotalIncome { get; set; }
+
+        public int TotalBooking { get; set; }
+        public int TotalRoom { get; set; }
+
+    }
+}
